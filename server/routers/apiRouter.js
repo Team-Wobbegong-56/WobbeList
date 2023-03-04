92edb698bd800b2a98ea7c477b1f8189566a0707
Fred Kim <104876440+Fredkim21@users.noreply.github.com>
2023-03-04 15:54:54 -0800

    updated apiRouter.js
    added sessionController to create new user

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -3,11 +3,12 @@
  */
 const express =  require('express');
 const userController = require('../controllers/userController');
+const sessionController = require('../controllers/sessionController');
 
 const UsersRouter = express.Router();
 
 // create a new user
-UsersRouter.post('/', userController.createUser);
+UsersRouter.post('/', userController.createUser, sessionController);
 
 // retrieve an existing user by username and password
 UsersRouter.post('/login', userController.getUser);
