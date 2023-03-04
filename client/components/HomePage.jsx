e086c597092e60124fb714130dd610dff4696280
Michelle Leong <leong.michellew@gmail.com>
2023-03-03 20:19:08 -0800

    basic homepage functionality

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
+import { Link, Routes, Route, useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const [location, setLocation] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const submitLocation = async (e) => {
+    e.preventDefault();
+    navigate('/:location');
+  };
+
   return (
-    <div>
-      <input></input>
+    <div id='home-page'>
+      <p>Where are you going? </p>
+      <input
+        type='text'
+        name='location'
+        id='location-search'
+        onChange={handleChange}
+        value={location}
+      />
+      <button id='submit' onClick={submitLocation} disabled={!location}>
+        Submit
+      </button>
     </div>
   );
 };
