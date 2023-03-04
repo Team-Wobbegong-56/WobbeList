f08373f7715bf807ab9a16f2828d3858476b1294
mattfookoo <matthewfukudome@gmail.com>
2023-03-03 19:18:56 -0800

    defined UserModels and ReviewModels, ready to be hooked up to mongo

---
+++ server/models/ReviewModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewsSchema = new Schema({
+  user_id: {
+    type: Schema.Types.ObjectId, // This is a reference to the User model defined in UserModel.js
+    ref: 'users', 
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  review_type: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: String,
+    required: false
+  }
+});
+
+module.exports = mongoose.model('reviews', ReviewsSchema);
